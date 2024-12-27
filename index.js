183a5b23ad2be697b3bf87a872e4b8d708dd6aab
Yashavant <yashavantkalashetti44@gmail.com>
2024-12-27 23:53:01 +0530

    8

--- index.js
+++ index.js
@@ -1,59 +1,29 @@
 require('dotenv').config();
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+const express = require("express")
+const http = require("http")
+const app = express()
+const server = http.createServer(app)
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "https://exam-temp.onrender.com",
+    methods: [ "GET", "POST" ]
+  }
+})
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id)
 
-const rooms = new Map();
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded")
+  })
 
-io.on('connection', (socket) => {
-  socket.on('join', ({ roomId, deviceType }) => {
-    socket.join(roomId);
-    
-    if (!rooms.has(roomId)) {
-      rooms.set(roomId, { laptop: null, phone: null });
-    }
-    
-    const room = rooms.get(roomId);
-    room[deviceType] = socket.id;
-    
-    console.log(`${deviceType} joined room: ${roomId}`);
-    
-    if (room.laptop && room.phone) {
-      io.to(roomId).emit('start-connection', { deviceType });
-    }
-  });
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+  })
 
-  socket.on('offer', (data) => {
-    socket.to(data.roomId).emit('offer', data.offer);
-  });
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal)
+  })
+})
 
-  socket.on('answer', (data) => {
-    socket.to(data.roomId).emit('answer', data.answer);
-  });
-
-  socket.on('ice-candidate', (data) => {
-    socket.to(data.roomId).emit('ice-candidate', data.candidate);
-  });
-
-  socket.on('disconnect', () => {
-    rooms.forEach((devices, roomId) => {
-      if (devices.laptop === socket.id) {
-        devices.laptop = null;
-        io.to(roomId).emit('device-disconnected', 'laptop');
-      }
-      if (devices.phone === socket.id) {
-        devices.phone = null;
-        io.to(roomId).emit('device-disconnected', 'phone');
-      }
-      if (!devices.laptop && !devices.phone) {
-        rooms.delete(roomId);
-      }
-    });
-  });
-});
-
-server.listen(process.env.PORT, () => console.log('Server running on port 3001'));
+server.listen(process.env.PORT, () => console.log("server is running on port 5000"))
