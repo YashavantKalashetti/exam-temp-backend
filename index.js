c014bfd1817fa6f1f676816745d36503bf7d57e5
Yashavant <yashavantkalashetti44@gmail.com>
2024-12-28 00:21:06 +0530

    9

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const server = http.createServer(app)
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "*",
+		origin: "https://exam-temp.onrender.com",
 		methods: [ "GET", "POST" ]
 	}
 })
