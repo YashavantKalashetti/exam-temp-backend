35bc1215244e20c05a51b1419fa7252b291498dc
Yashavant <yashavantkalashetti44@gmail.com>
2024-12-27 16:34:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('offer', (offer) => {
+    socket.broadcast.emit('offer', offer);
+  });
+
+  socket.on('answer', (answer) => {
+    socket.broadcast.emit('answer', answer);
+  });
+
+  socket.on('ice-candidate', (candidate) => {
+    socket.broadcast.emit('ice-candidate', candidate);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected:', socket.id);
+  });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log('Signaling server running on http://localhost:3001');
+});
